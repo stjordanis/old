2f9d8a75c0e278119aa1026b752cf25ca88b4d32
Kevin Serrano <kevgagser@gmail.com>
2020-12-22 16:53:49 -0800

    chore: lay foundation for nuxt content migration.

---
+++ .nuxt/router.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from '@nuxt/ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _6bb15c7f = () => interopDefault(import('..\\node_modules\\@nuxt\\content-theme-docs\\src\\pages\\releases.vue' /* webpackChunkName: "pages/releases" */))
+const _03242867 = () => interopDefault(import('..\\node_modules\\@nuxt\\content-theme-docs\\src\\pages\\_slug.vue' /* webpackChunkName: "pages/_slug" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/releases",
+    component: _6bb15c7f,
+    name: "releases___en"
+  }, {
+    path: "/:slug?",
+    component: _03242867,
+    name: "slug___en"
+  }],
+
+  fallback: false
+}
+
+function decodeObj(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'string') {
+      obj[key] = decode(obj[key])
+    }
+  }
+}
+
+export function createRouter () {
+  const router = new Router(routerOptions)
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    const r = resolve(to, current, append)
+    if (r && r.resolved && r.resolved.query) {
+      decodeObj(r.resolved.query)
+    }
+    return r
+  }
+
+  return router
+}
---
+++ content/en/index.md
@@ -0,0 +1,19 @@
+---
+title: "Introduction"
+description: "Collaborate, create, and connect in an all-in-one data processing suite!"
+position: 1
+category: "Getting Started"
+version: 1.4
+fullscreen: false
+menuTitle: "Intro"
+---
+
+Hello hello
+
+## Section 1
+
+### Subsection 1
+
+## Section 2
+
+## Section 3
