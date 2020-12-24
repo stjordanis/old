95488962959dadd6a29c6a5d5cc1365533bd9f8e
Justin Brooks <justin@datatorch.io>
2020-12-23 22:16:01 -0500

    chore: generate sitemap

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,5 +1,20 @@
-import theme from "@nuxt/content-theme-docs";
+import theme from '@nuxt/content-theme-docs'
+
+const createSitemapRoutes = async () => {
+  const { $content } = require('@nuxt/content')
+  const files = await $content('/', { deep: true })
+    .only('to')
+    .fetch()
+  const ignore = ['/settings']
+  return files.filter(f => !ignore.includes(f.path)).map(file => file.to)
+}
 
 export default theme({
-  docs: { primaryColor: "#eb4b1a" },
-});
+  docs: { primaryColor: '#eb4b1a' },
+  modules: ['@nuxtjs/sitemap'],
+  sitemap: {
+    hostname: 'https://docs.datatorch.io',
+    gzip: true,
+    routes: createSitemapRoutes
+  }
+})
