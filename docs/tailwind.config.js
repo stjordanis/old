32777f1bfe47aa10e9c582dc27aa8cd8ecb36804
Kevin Serrano <kevgagser@gmail.com>
2021-01-21 10:58:38 -0800

    refactor: change safelist into regexx.

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,17 +1,9 @@
 module.exports = {
   purge: {
     options: {
-      safelist: [
-        'bg-blue-100',
-        'dark:bg-blue-900',
-        'text-blue-500',
-        'bg-purple-100',
-        'dark:bg-purple-900',
-        'text-purple-500',
-        'bg-green-100',
-        'dark:bg-green-900',
-        'text-green-500'
-      ]
+      safelist: {
+        standard: [/bg-\w+-100/, /dark\:bg-\w+-900/, /text-\w+-500/]
+      }
     }
   }
 }
